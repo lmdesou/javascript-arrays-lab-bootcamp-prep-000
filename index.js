9ac6eea819e09f92aa684e9198a43ead21ad5b7b
Lalita Desouza <lmdesou@yahoo.com>
2017-10-28 22:26:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,5 +30,6 @@ function removeLastKitten() {
   return newKittens
 }
 function removeFirstKitten() {
-  var newKittens = kittens.slice(1)
+  var newKittens = kittens.slice(1);
+  return newKittens
 }
