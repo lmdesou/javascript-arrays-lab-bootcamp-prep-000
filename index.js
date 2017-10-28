a6193e6ff4747b66fff09f3c27f47e91a6cede4d
Lalita Desouza <lmdesou@yahoo.com>
2017-10-28 22:19:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const app = "I don't do much."
 kittens = ["Milo", "Otis", "Garfield"]
 
 //changes array
-function destructivelyAppendKitten(Ralph) {
-  return kittens.push(Ralph);
+function destructivelyAppendKitten(name) {
+  return kittens.push(name);
 }
